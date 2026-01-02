2ebe7ace6435872ed6aed061a58ed8814d99257e
mohommedlikesapplejuice <119367371+mohommedlikesapplejuice@users.noreply.github.com>
2026-01-02 09:45:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('display-box');
+
+const appendToDisplay = (input) =>{
+    display.value += input;
+}
+
+const calculate = () =>{
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = `Error in Syntax`
+    }
+}
+
+const clearDisplay = () =>{
+     dispplay.value = '';
+}
